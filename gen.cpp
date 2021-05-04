01e5c8dce0790a0df7f6a3b747d196e7e42361f6
Anubhav Mishra <67906361+Anubhav495@users.noreply.github.com>
2021-05-04 10:29:53 +0530

    Add files via upload

---
+++ gen.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+#define all(a) a.begin(),a.end()	
+#define rev(a) a.rbegin(),a.rend()	
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; //less_equal for multiset
+#define ar array
+#define pb push_back
+#define fi(a,b) for(int i=a;i<(b);i++)
+#define fj(a,b) for(int j=a;j<(b);j++)
+#define fk(a,b) for(int k=a;k<(b);k++)
+const double pi=acosl(-1);
+
+int main(int argc,char* argv[])
+{
+	FAST;
+	srand(atoi(argv[1]));
+	int number_of_points=50+rand()%40;
+	fi(0,number_of_points)
+	{
+		int x=5+rand()%100;
+		int y=4+rand()%100;
+		cout<<i<<' '<<x<<' '<<y<<'\n';
+	}
+
+	
+	
+}
